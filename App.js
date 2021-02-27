7be77745756995a0af1b91bd7c9b26e639fd5c34
Chriswelin <79392422+Chriswelin@users.noreply.github.com>
2021-02-26 21:09:26 -0800

    Update App.js

--- App.js
+++ App.js
@@ -2,14 +2,17 @@ import './App.css';
 import Footer from './Components/Footer.js'
 import Main from './Components/Main.js'
 import Header from  './Components/Header.js'
- 
+import Textfields from './Components/Textfields.js'
+import img from './Components/star-circles.jpg' 
 
 function App() {
   return (
     <div>
-        <Header/>
-        <Main/>
-        <Footer/>
+        <Header />
+        <img className = "image" src={img}/>          
+        <Main />
+        <Textfields />
+        <Footer />
     </div>
   );
 }
